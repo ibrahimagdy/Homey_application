3161aefa47a95e2e72d192b8ec267207b9d900ed
Ibrahim <iibbrraahhimm00@gmai;.com>
2024-05-06 06:14:45 +0200

    notifications

--- lib/pages/notifications_screen/notifications_screen.dart
+++ lib/pages/notifications_screen/notifications_screen.dart
@@ -1,136 +1,30 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:homey/pages/notifications_screen/widgets/notification_item.dart';
 
-class NotificationScreen extends StatefulWidget {
+class NotificationScreen extends StatelessWidget {
   static const String routeName = "not";
+  final RemoteMessage message;
 
-  const NotificationScreen({super.key});
-
-  @override
-  State<NotificationScreen> createState() => _NotificationScreenState();
-}
-
-class _NotificationScreenState extends State<NotificationScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-  final List<NotificationData> notifications = [];
-
-  Future<void> initializeNotifications() async {
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('icon');
-    const InitializationSettings initializationSettings =
-        InitializationSettings(android: initializationSettingsAndroid);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-  }
-
-  Future<void> handleNotification(RemoteMessage message) async {
-    final RemoteNotification? notification = message.notification;
-    final AndroidNotification? android = message.notification?.android;
-
-    if (notification != null && android != null) {
-      final String title = notification.title ?? '';
-      final String body = notification.body ?? '';
-
-      await firestore.collection('Notifications').add({
-        'title': title,
-        'body': body,
-      });
-
-      setState(() {
-        notifications.add(NotificationData(title: title, body: body));
-      });
-
-      showNotification(notification);
-    }
-  }
-
-  Future<void> showNotification(RemoteNotification notification) async {
-    const AndroidNotificationChannel channel = AndroidNotificationChannel(
-      'high_importance_channel',
-      'High Importance Notifications',
-      description: 'This channel is used for important notifications.',
-      importance: Importance.max,
-    );
-
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-
-    final int notificationId = notification.hashCode;
-    final String title = notification.title ?? '';
-    final String body = notification.body ?? '';
-
-    await flutterLocalNotificationsPlugin.show(
-      notificationId,
-      title,
-      body,
-      NotificationDetails(
-        android: AndroidNotificationDetails(
-          channel.id,
-          channel.name,
-          channelDescription: channel.description,
-          icon: 'icon',
-        ),
-      ),
-    );
-  }
+  const NotificationScreen({required this.message, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       backgroundColor: const Color(0xff0A1128),
-      body: ListView.builder(
-        itemCount: notifications.length,
-        itemBuilder: (context, index) {
-          final NotificationData notification = notifications[index];
-          return Container(
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  notification.title,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  notification.body,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
+      appBar: AppBar(
+        backgroundColor: const Color(0xff0A1128),
+        leading: IconButton(
+          color: const Color(0xffC6FAFF),
+          iconSize: 32,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(CupertinoIcons.back),
+        ),
       ),
+      body: NotificationItem(message: message),
     );
   }
-}
-
-class NotificationData {
-  final String title;
-  final String body;
-
-  NotificationData({required this.title, required this.body});
 }
