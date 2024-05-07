97042609549f6518af122c66bf1f8eb8cce11888
Ibrahim <iibbrraahhimm00@gmai;.com>
2024-05-07 04:28:56 +0200

    update notifications screen

--- lib/pages/notifications_screen/models/notification_model.dart
+++ lib/pages/notifications_screen/models/notification_model.dart
@@ -6,21 +6,5 @@ class NotificationModel {
 
   NotificationModel({this.dataTitle, this.dataBody, this.title, this.body});
 
-  factory NotificationModel.fromJson(Map<String, dynamic> json) {
-    return NotificationModel(
-      title: json['title'],
-      body: json['body'],
-      dataTitle: json['dataTitle'],
-      dataBody: json['dataBody'],
-    );
-  }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'title': title,
-      'body': body,
-      'dataTitle': dataTitle,
-      'dataBody': dataBody,
-    };
-  }
 }
