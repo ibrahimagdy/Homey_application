83c9462a0486c198eb2a4dac60aef269e0f0de84
Ibrahim Magdy <iibbrraahhimm00@gmail.com>
2024-05-10 04:07:47 +0300

    update map view

--- lib/pages/map_view/map_view.dart
+++ lib/pages/map_view/map_view.dart
@@ -17,46 +17,49 @@ class _MapViewState extends State<MapView> {
       idLocation: "1",
       titleLocation: "Near from Mansoura University",
       latLng: LatLng(31.036605430944128, 31.361358885174905),
+      price: "3650 EGP/month",
     ),
     const MapModel(
       idLocation: "2",
       titleLocation: "Near from German University",
       latLng: LatLng(29.996348671789004, 31.438558240111757),
+      price: "4700 EGP/month",
     ),
     const MapModel(
       idLocation: "3",
       titleLocation: "Near from Alexandria University",
       latLng: LatLng(31.21002324037778, 29.91407565718379),
-    ),
-    const MapModel(
-      idLocation: "3",
-      titleLocation: "Near from Alexandria University",
-      latLng: LatLng(31.21002324037778, 29.91407565718379),
+      price: "3500 EGP/month",
     ),
     const MapModel(
       idLocation: "4",
       titleLocation: "Near from Sadat Academy",
       latLng: LatLng(29.966811431738726, 31.256733021994652),
+      price: "3000 EGP/month",
     ),
     const MapModel(
       idLocation: "5",
       titleLocation: "Near from Fayoum University",
       latLng: LatLng(29.318981199265618, 30.83824800231381),
+      price: "1300 EGP/month",
     ),
     const MapModel(
       idLocation: "6",
       titleLocation: "Near from Tanta University",
       latLng: LatLng(30.79412967707392, 30.99340773904518),
+      price: "2000 EGP/month",
     ),
     const MapModel(
       idLocation: "7",
       titleLocation: "Near from KFS University",
       latLng: LatLng(31.09520834123604, 30.945486394585412),
+      price: "3450 EGP/month",
     ),
     const MapModel(
       idLocation: "8",
       titleLocation: "Near from Menofia University",
       latLng: LatLng(30.567476924839806, 31.01079039395229),
+      price: "2900 EGP/month",
     ),
   ];
 
@@ -86,6 +89,10 @@ class _MapViewState extends State<MapView> {
     var myMarkers = apartments
         .map(
           (apartmentModel) => Marker(
+            infoWindow: InfoWindow(
+              title: apartmentModel.titleLocation,
+              snippet: apartmentModel.price,
+            ),
             position: apartmentModel.latLng,
             markerId: MarkerId(
               apartmentModel.idLocation.toString(),
--- lib/pages/map_view/models/map_model.dart
+++ lib/pages/map_view/models/map_model.dart
@@ -4,9 +4,12 @@ class MapModel {
   final LatLng latLng;
   final String idLocation;
   final String titleLocation;
+  final String price;
 
-  const MapModel(
-      {required this.titleLocation,
-      required this.idLocation,
-      required this.latLng});
+  const MapModel({
+    required this.titleLocation,
+    required this.idLocation,
+    required this.latLng,
+    required this.price,
+  });
 }
